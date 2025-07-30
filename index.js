7e7c6ede8c91af8760b29c6460fbda37c3f7b0d2
Aditi-Deshmukh-3012 <deshmukhadit3012@gmail.com>
2025-07-30 17:09:29 +0530

    commit

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const app = express();
 const port = 3000;
 const path = require("path");
 const mysql = require("mysql2");
-
-
+const bcrypt = require("bcrypt");
+const session = require("express-session");
+const flash = require("connect-flash");
+const engine = require("ejs-mate");
+app.engine("ejs",engine);
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
@@ -18,6 +21,7 @@ db.connect((err) => {
 });
 
 
+
 app.use(express.urlencoded({extended:true}));
 app.set("view engine" , "ejs");
 app.set("views", path.join(__dirname,"views"));
@@ -26,6 +30,31 @@ const methodOverride = require("method-override");
 app.use(methodOverride("_method"));
 const { v4: uuidv4 } = require('uuid');
 
+
+app.use(session({
+    secret:"abcd",
+    resave:false,
+    saveUninitialized:false
+}));
+
+app.use(flash());
+
+app.use((req, res, next) => {
+    res.locals.success = req.flash("success");
+    res.locals.error = req.flash("error");
+    res.locals.username= req.session.username || null;
+    next();
+});
+
+
+function requirelogin(req,res,next){
+    if(!req.session.userid){
+        // console.log("hehe");
+        return res.redirect("/login");
+    }
+    next();
+}
+// app.use(requirelogin);
 
 // let posts = [
 //     {
@@ -48,6 +77,77 @@ const { v4: uuidv4 } = require('uuid');
 //     res.render("index.ejs",{posts});
 // }) 
 
+app.get("/signup",(req,res)=>{
+    res.render("signup.ejs");
+});
+app.post("/signup",async (req,res)=>{
+    console.log(req.body);
+    const {username , password} = req.body;
+  
+    try{
+    const hashpassword = await bcrypt.hash(password,10);
+    const sql = 'insert into users(username,password) values (?,?)';
+    db.query(sql,[username,hashpassword],(err,result)=>{
+        // if(err){
+        //     console.log(err);
+        //     return res.send('error during signup');
+        // }
+        // // console.log(result);
+        // res.send('user registered successfully! <a href="/login">Login Here</a>');
+
+        if (err) {
+          console.log(err);
+           req.flash('error', 'Error during signup');
+           return res.redirect("/signup");
+         }
+        req.flash('success', 'User registered successfully! Please login.');
+        res.redirect("/login");
+    });
+}catch(err){
+    console.log(err);
+    res.send("something went wrong");
+}
+
+   
+
+});
+
+app.get("/login",(req,res)=>{
+    res.render("login.ejs");
+});
+
+app.post("/login",(req,res)=>{
+    const {username , password} = req.body;
+
+    const sql = 'select * from users where username = ?';
+    db.query(sql,[username],async(err,result)=>{
+        if(err){
+            console.log(err);
+            req.flash("error","error during login");
+            return res.redirect("/login");
+        }
+        if(result.length==0){
+            req.flash("error","User not found");
+            return res.redirect("/login");
+        }
+
+        const user = result[0];
+        const ismatch = await bcrypt.compare(password,user.password);
+        if(!ismatch){
+            req.flash("error","Incorrect Password");
+            return res.redirect("/login");
+
+        }
+        // console.log(req);
+        req.session.userid = user.id;
+        req.session.username = username;
+        // console.log(req);
+        req.flash("success","Login Successful!");
+        res.redirect("/posts");
+    });
+});
+
+
 app.get("/posts", (req, res) => {
     db.query("SELECT * FROM posts", (err, results) => {
         if (err) throw err;
@@ -57,9 +157,12 @@ app.get("/posts", (req, res) => {
 
 
 
-app.get("/posts/new" ,(req,res)=>{
-    res.render("new.ejs");
-})
+app.get("/posts/new" ,requirelogin,(req,res)=>{
+    const obj ={
+        user : req.session.username
+    }
+    res.render("new.ejs" , obj);
+});
 
 // app.post("/posts",(req,res)=>{
     
@@ -74,8 +177,9 @@ app.get("/posts/new" ,(req,res)=>{
 //     res.redirect("/posts");
 // });
 
-app.post("/posts", (req, res) => {
-    const { username, content } = req.body;
+app.post("/posts",requirelogin, (req, res) => {
+    const username = req.session.username;
+    const { content } = req.body;
     const id = uuidv4();
     const q = "INSERT INTO posts (id, username, content) VALUES (?, ?, ?)";
     db.query(q, [id, username, content], (err, result) => {
@@ -119,10 +223,15 @@ app.get("/posts/view/:id", (req, res) => {
 // });
 
 
-app.get("/posts/edit/:id", (req, res) => {
+app.get("/posts/edit/:id",requirelogin, (req, res) => {
     const { id } = req.params;
     db.query("SELECT * FROM posts WHERE id = ?", [id], (err, results) => {
         if (err) throw err;
+        const post = results[0];
+        if(post.username != req.session.username){
+            req.flash("error","unauthorized. You can only edit your own posts!!");
+            return res.redirect("/posts");
+        }
         res.render("edit.ejs", { obj: results[0] });
     });
 });
@@ -143,14 +252,28 @@ app.get("/posts/edit/:id", (req, res) => {
 //     res.redirect("/posts");
 // });
 
-app.patch("/posts/:id", (req, res) => {
+app.patch("/posts/:id", requirelogin,(req, res) => {
     const { id } = req.params;
     const { content } = req.body;
-    const q = "UPDATE posts SET content = ? WHERE id = ?";
-    db.query(q, [content, id], (err, result) => {
-        if (err) throw err;
-        res.redirect("/posts");
+    
+    const sel_q = "SELECT * from posts where id = ?";
+    db.query(sel_q,[id],(err,result)=>{
+        if(err){
+            throw err;
+        }
+        const post = result[0];
+        
+        if(post.username != req.session.username){
+            req.flash("error","Unathuraized. only the owner of the post can edit. ")
+            return res.redirect("/posts");
+        }
+        const up_q = "UPDATE posts SET content = ? WHERE id = ?";
+        db.query(up_q, [content, id], (err, result) => {
+            if (err) throw err;
+            res.redirect("/posts");
+        });
     });
+    
 });
 
 
@@ -167,13 +290,39 @@ app.patch("/posts/:id", (req, res) => {
 //    res.redirect("/posts");
 // });
 
-app.delete("/posts/:id", (req, res) => {
+app.delete("/posts/:id",requirelogin, (req, res) => {
     const { id } = req.params;
-    const q = "DELETE FROM posts WHERE id = ?";
-    db.query(q, [id], (err, result) => {
-        if (err) throw err;
-        res.redirect("/posts");
+
+    const sel_q = "SELECT * from posts where id = ?";
+    db.query(sel_q,[id],(err,result)=>{
+        if(err){
+            throw err;
+        }
+        const post = result[0];
+        if(post.username != req.session.username){
+           req.flash("error","Unathuraized. only the owner of the post can delete. ")
+           return res.redirect("/posts");
+        }
+        const del_q = "DELETE FROM posts WHERE id = ?";
+        db.query(del_q, [id], (err, result) => {
+            if (err) throw err;
+            res.redirect("/posts");
+        });
     });
+
+});
+
+app.get("/logout",requirelogin,(req,res)=>{
+    req.flash("success","logged out successfully!");
+    req.session.destroy((err)=>{
+        if(err){
+            console.log(err);
+            req.flash("error","error while logging out!");
+            return res.redirect("/posts");
+        }
+    res.redirect("/login");
+
+   });
 });
 
 app.listen(port,()=>{
